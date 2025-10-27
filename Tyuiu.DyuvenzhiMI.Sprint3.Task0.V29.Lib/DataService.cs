4d3754eaa730fb364787813ad118bcb9dd53e9e3
DyuvenzhiMaxim <Tyuiu.DyuvenzhiMI@outlook.com>
2025-10-27 13:20:58 +0500

    Add project files.

---
+++ Tyuiu.DyuvenzhiMI.Sprint3.Task0.V29.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿
+using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.DyuvenzhiMI.Sprint3.Task0.V29.Lib
+{
+    public class DataService : ISprint3Task0V29
+    {
+        public double GetSumSeries(double value, int startValue, int stopValue)
+        {
+            double S = 0;
+            int k;
+            for (k = startValue; k <= stopValue; k++)
+            {
+                double V = (Math.Pow(value, (2 * k)) + 1.0 / (k + 1)) * Math.Cos(value);
+                S += V;
+            }
+            return Math.Round(S, 3);
+        }
+    }
+}
---
+++ Tyuiu.DyuvenzhiMI.Sprint3.Task0.V29.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿
+using Tyuiu.DyuvenzhiMI.Sprint3.Task0.V29.Lib;
+
+namespace Tyuiu.DyuvenzhiMI.Sprint3.Task0.V29.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckValid()
+        {
+            DataService ds = new DataService();
+            double value = 0.5;
+            int startValue = 1;
+            int stopValue = 10;
+            double res = ds.GetSumSeries(value, startValue, stopValue);
+            double wait = 2.065;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
