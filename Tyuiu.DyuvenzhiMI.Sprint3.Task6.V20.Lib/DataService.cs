178fa3f05746fd76380ae98d4c08c0d8462ac5fd
DyuvenzhiMaxim <Tyuiu.DyuvenzhiMI@outlook.com>
2025-10-28 23:19:47 +0500

    Task6

---
+++ Tyuiu.DyuvenzhiMI.Sprint3.Task6.V20.Lib/DataService.cs
@@ -0,0 +1,21 @@
+﻿
+using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.DyuvenzhiMI.Sprint3.Task6.V20.Lib
+{
+    public class DataService : ISprint3Task6V20
+    {
+        public int GetSumTheDivisors(int startValue, int stopValue)
+        {
+            int count = 0;
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                for (int y = 1; y <= x; y++)
+                {
+                    if (y < 12) {  count++; }
+                }
+            }
+            return count;
+        }
+    }
+}
---
+++ Tyuiu.DyuvenzhiMI.Sprint3.Task6.V20.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿
+using Tyuiu.DyuvenzhiMI.Sprint3.Task6.V20.Lib;
+
+namespace Tyuiu.DyuvenzhiMI.Sprint3.Task6.V20.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int startValue = 20;
+            int stopValue = 32;
+            int res = ds.GetSumTheDivisors(startValue, stopValue);
+            Assert.AreEqual(143, res);
+        }
+    }
+}
